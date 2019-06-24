5554c67c24b0a4f1406b627686c32487ab340b5f
Raisa Primerova (Раиса Примерова) <riyuhide@gmail.com>
2019-06-24 12:36:20 -0600

    working code - first part

--- www/modules/about/about.php
+++ www/modules/about/about.php
@@ -3,8 +3,8 @@
 
 
 
-include ROOT . 'views/parts/header.tpl';
-include ROOT . 'views/pages/about.tpl';
-include ROOT . 'views/parts/footer.tpl';
+include ROOT . '\views\parts\header.tpl';
+include ROOT . '\views\pages\about.tpl';
+include ROOT . '\views\parts\footer.tpl';
 
 ?>
--- www/modules/contacts/contacts.php
+++ www/modules/contacts/contacts.php
@@ -3,8 +3,8 @@
 
 
 
-include ROOT . 'views/parts/header.tpl';
-include ROOT . 'views/pages/contacts.tpl';
-include ROOT . 'views/parts/footer.tpl';
+include ROOT . '\views\parts\header.tpl';
+include ROOT . '\views\pages\contacts.tpl';
+include ROOT . '\views\parts\footer.tpl';
 
 ?>
